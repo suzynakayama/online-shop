314eb836da6024ebce90de8be9104c8ae2924cb2
Suzy <suzy.nakayama@gmail.com>
2020-04-03 18:16:44 -0400

    first commit

---
+++ src/pages/HomePage.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const HomePage = () => {
+	return (
+		<div className="homepage">
+			<div className="directory">
+				<div className="directory__item">
+					<div className="directory__item--content">
+						<h1 classname="directory__item--title">WOMEN</h1>
+						<span className="directory__item--subtitle">Shop</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default HomePage;

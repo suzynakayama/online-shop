1f946488cf8b9d7c627fc0453a2f4abf4042344f
Suzy <suzy.nakayama@gmail.com>
2020-04-03 20:22:34 -0400

    added shop data and images

--- src/components/Directory/Directory.jsx
+++ src/components/Directory/Directory.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import DirectoryItem from "../DirectoryItem/DirectoryItem";
-import { allSections } from "../../data/sections";
+import SECTIONS_DATA from "../../data/sections";
 import "./Directory.styles.scss";
 
 const Directory = () => {
-	const [sections, setSections] = useState(allSections);
+	const [sections, setSections] = useState(SECTIONS_DATA);
 
 	return (
 		<div className="directory">
